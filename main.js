4b55cf8c264a3289c8e2478ec97368eed4efbfd9
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-14 02:05:37 -0400

    script working

--- main.js
+++ main.js
@@ -1,3 +1,71 @@
-import './style.css'
 
 
+
+        const resultList = document.querySelector('#results')
+        
+        let flip = '';
+        let spin = '';
+        let stance = '';
+ 
+       
+      
+            function creatLine(){
+            new URLSearchParams(window.location.search).forEach((value,name) => {
+                let trickSentence = document.createElement('p');
+                trickSentence.className = 'trickSentence';
+                if (name === "stance" && value === "on"){
+                  spinSelector = Math.floor((Math.random() * 4));
+                  console.log(spinSelector)
+                  switch (spinSelector){
+                      case 0: stance = 'Regular';
+                      break;
+                      case 1: stance = 'Switch';
+                      break;  
+                      case 2: stance = 'Nollie';
+                      break;
+                      case 3: stance = 'Fackie';
+                      break;    
+                    }  
+               }
+               if (name === "spin" && value === "on"){
+                  spinSelector = Math.floor((Math.random() * 4));
+                  console.log(spinSelector)
+                  switch (spinSelector){
+                      case 0: spin = 'Fs 180';
+                      break;
+                      case 1: spin = 'Bs 180';
+                      break;  
+                      case 2: spin = 'Fs 360';
+                      break;
+                      case 3: spin = 'Bs 360';
+                      break;    
+                    }  
+               }
+               if(name === "flip" && value === "on"){
+                  flipSelector = Math.floor((Math.random() * 5));
+                  console.log(flipSelector)
+                  switch (flipSelector){
+                      case 0: flip = 'Kickflip';
+                      break;
+                      case 1: flip = 'Variel-Kickflip';
+                      break;
+                      case 2: flip = 'TreFlip'; 
+                      break;
+                      case 3: flip = 'Heelflip';
+                      break;
+                      case 4: flip = 'Variel-Heelflip';
+                      break;
+                      case 5: flip = 'LazerFlip';
+                      break;
+                    }
+               }
+               trickSentence.textContent = `${stance} ${spin} ${flip}`;
+               resultList.appendChild(trickSentence);
+            })
+        }
+        document.querySelector('.submit').addEventListener('click', Event => {
+            Event.preventDefault();
+            creatLine();
+        })
+        
+        resultList.append(document.createElement('br'))
