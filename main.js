1103a5d3551b66a5464d8b540b93feb1bb8a20b8
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-14 20:20:57 -0400

    v.2 trying to get a selected number to only append

--- main.js
+++ main.js
@@ -1,19 +1,32 @@
-const resultList = document.querySelector("#results");
-
-let flip = document.createElement('p');
-
-let spin = "";
-let stance = "";
-
-
+let flip = '';
+let numberOfTricks = 0;
 new URLSearchParams(window.location.search).forEach((value, name) => {
-    console.log(name)
-   console.log(value)
-   if(name === 'flip' && value === 'on'){
-    let randomNumber = Math.floor(Math.random()*3)  
-    console.log(randomNumber)
-    flip.textContent = 'kickflip';
-        document.querySelector('#results').append(flip);
+  console.log(`${name}: ${value}`);
+
+  if (name === "numberOfTricks" && value === "one") {
+    numberOfTricks = 0;
+    console.log("this is a one");
+  } else if (name === "numberOfTricks" && value === "two") {
+    numberOfTricks = 1;
+    console.log("this is a two");
+  } else if (name === "numberOfTricks" && value === "three") {
+    numberOfTricks = 2;
+    console.log("this is a three");
   }
+  for (let i = 0; i <= numberOfTricks; i++) {
+    let randomNumber = 0;
+    if (name === "flip" && value === "on") {
+      randomNumber = Math.floor(Math.random() * 3);
+      console.log(randomNumber);
+      if(randomNumber === 0){
+          flip = 'kickflip'
+      }else if(randomNumber === 1){
+        flip = 'heelflip'
+      }else if(randomNumber === 2){
+          flip = 'treflip'
+      }
+  }
+  document.querySelector('#results').append(`${flip}`)
+}
 
 });
