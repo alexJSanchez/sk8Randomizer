26c747ac178087af1aa35649e8af2bb3483f0987
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-16 11:52:12 -0400

    v.2 created funtions to seperate actions

--- main.js
+++ main.js
@@ -1,32 +1,55 @@
-let flip = '';
+let flip = document.createElement('p');
+flip.className = "flip";
 let numberOfTricks = 0;
+
+
+function randomNumber(){
+  let randomNumber = Math.floor(Math.random() * 3);
+  return randomNumber;
+}
+function flipFunction(randomNumber){
+  if (randomNumber === 0) {
+    document.getElementsByClassName('flip').textContent = "kickflip";
+  } else if (randomNumber === 1) {
+    flip.textContent = "heelflip";
+  } else if (randomNumber === 2) {
+    flip.textContent = "treflip";
+  } 
+  return flip
+}
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  if (name === "numberOfTricks" && value === "one") {
+    numberOfTricks = 1;
+    flipFunction(randomNumber);
+    console.log(flip)
+    console.log("this is a one");
+  } else if (name === "numberOfTricks" && value === "two") {
+    numberOfTricks = 2;
+    console.log("this is a two");
+  } else if (name === "numberOfTricks" && value === "three") {
+    numberOfTricks = 3;
+    console.log("this is a three");
+  }
+  });
+
 new URLSearchParams(window.location.search).forEach((value, name) => {
   console.log(`${name}: ${value}`);
 
-  if (name === "numberOfTricks" && value === "one") {
-    numberOfTricks = 0;
-    console.log("this is a one");
-  } else if (name === "numberOfTricks" && value === "two") {
-    numberOfTricks = 1;
-    console.log("this is a two");
-  } else if (name === "numberOfTricks" && value === "three") {
-    numberOfTricks = 2;
-    console.log("this is a three");
+  if (name === "flip" && value === "on" && name === "numberOfTricks" && value === "one") {
+     flipFunction(randomNumber);
+    document.querySelector('#results').append(`${flip}`)
+  }else if(name === "flip" && value === "on" && name === "numberOfTricks" && value === "one"){
+    flipFunction(randomNumber);
+    document.querySelector('#results').append(`${flip}`)
+    flipFunction(randomNumber)
+    document.append('#results').append(`${flip}`);
   }
-  for (let i = 0; i <= numberOfTricks; i++) {
-    let randomNumber = 0;
-    if (name === "flip" && value === "on") {
-      randomNumber = Math.floor(Math.random() * 3);
-      console.log(randomNumber);
-      if(randomNumber === 0){
-          flip = 'kickflip'
-      }else if(randomNumber === 1){
-        flip = 'heelflip'
-      }else if(randomNumber === 2){
-          flip = 'treflip'
-      }
-  }
-  document.querySelector('#results').append(`${flip}`)
-}
-
 });
+
+
+if (numberOfTricks = 1){
+
+  document.querySelector('#result')
+}
+  
