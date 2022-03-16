c3563cbcbe7688db64902eda303d0928b9a36e8e
MrSamich <65629448+MrSamich@users.noreply.github.com>
2022-03-16 14:56:39 -0400

    v.3 got a spin return

--- main.js
+++ main.js
@@ -1,22 +1,44 @@
 let resultsContainer = document.querySelector('#results');
-
+ 
 let numberOfTricks = 0;
 
 
-function randomNumber(){
+function flipRandomNumber(){
   let randomNumber = 0;
-  randomNumber = Math.floor(Math.random() * 3);
+  randomNumber = Math.floor(Math.random() * 6);
   return randomNumber;
 }
-function flipFunction(randomNumber){
+function spinRandomNumber(){
+  let spinRandomNumber = 0;
+  spinRandomNumber = Math.floor(Math.random() * 2);
+  return spinRandomNumber;
+}
+function spinFunction(spinRandomNumber){
+  let spin = document.createElement('p');
+  spin.className = "spin";
+  if (spinRandomNumber === 0) {
+    spin.textContent = "180";
+  } else if (spinRandomNumber === 1) {
+    spin.textContent = "360";
+  } 
+  return spin;
+}
+
+function flipFunction(flipRandomNumber){
   let flip = document.createElement('p');
   flip.className = "flip";
-  if (randomNumber === 0) {
-    flip.textContent = "kickflip";
-  } else if (randomNumber === 1) {
-    flip.textContent = "heelflip";
-  } else if (randomNumber === 2) {
-    flip.textContent = "treflip";
+  if (flipRandomNumber === 0) {
+    flip.textContent = "Kickflip";
+  } else if (flipRandomNumber === 1) {
+    flip.textContent = "Heelflip";
+  } else if (flipRandomNumber === 2) {
+    flip.textContent = "Treflip";
+  } else if (flipRandomNumber === 3) {
+    flip.textContent = "Variel-Heel";
+  } else if (flipRandomNumber === 4) {
+    flip.textContent = "Variel-Flip";
+  } else if (flipRandomNumber === 5) {
+    flip.textContent = "Lazer-Flip";
   } 
   return flip;
 }
@@ -33,15 +55,29 @@ new URLSearchParams(window.location.search).forEach((value, name) => {
 
 new URLSearchParams(window.location.search).forEach((value, name) => {
   console.log(`${name}: ${value}`);
-  if (name === "flip" && value === "on" && numberOfTricks === 0) {
-    resultsContainer.append(flipFunction(randomNumber()));
+  if (name === "spin" && value === "on" && numberOfTricks === 0) {
+   resultsContainer.append(spinFunction(spinRandomNumber()));
   }else if(name === "flip" && value === "on" && numberOfTricks === 1){
-    resultsContainer.append(flipFunction(randomNumber()));
-    resultsContainer.append(flipFunction(randomNumber()));
+    resultsContainer.append(spinFunction(spinRandomNumber()));
+    resultsContainer.append(spinFunction(spinRandomNumber()));
   }else if(name === "flip" && value === "on" && numberOfTricks === 2){
-    resultsContainer.append(flipFunction(randomNumber()));
-    resultsContainer.append(flipFunction(randomNumber()));
-    resultsContainer.append(flipFunction(randomNumber()));
+    resultsContainer.append(spinFunction(spinRandomNumber()));
+    resultsContainer.append(spinFunction(spinRandomNumber()));
+    resultsContainer.append(spinFunction(spinRandomNumber()));
+  }
+});
+
+new URLSearchParams(window.location.search).forEach((value, name) => {
+  console.log(`${name}: ${value}`);
+  if (name === "flip" && value === "on" && numberOfTricks === 0) {
+   resultsContainer.append(flipFunction(flipRandomNumber()));
+  }else if(name === "flip" && value === "on" && numberOfTricks === 1){
+    resultsContainer.append(flipFunction(flipRandomNumber()));
+    resultsContainer.append(flipFunction(flipRandomNumber()));
+  }else if(name === "flip" && value === "on" && numberOfTricks === 2){
+    resultsContainer.append(flipFunction(flipRandomNumber()));
+    resultsContainer.append(flipFunction(flipRandomNumber()));
+    resultsContainer.append(flipFunction(flipRandomNumber()));
   }
 });
 
